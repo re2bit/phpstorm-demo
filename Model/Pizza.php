309e6519d4c9bcf5be17b67ce19d88b7f230253b
Rene Gerritsen <rene.gerritsen@check24.de>
2017-05-23 14:05:21 +0200

    add pizza model

---
+++ Model/Pizza.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Model;
+
+
+/**
+ * Pizza ! nothing more to say !
+ *
+ * @copyright CHECK24 Vergleichsportal Reise GmbH
+ */
+class Pizza
+{
+    private $temp; //@todo: refactor me, rename me
+    private $ingediants; //@todo: refactor me, rename me With alt+enter
+    private $number_of_slices;
+}
