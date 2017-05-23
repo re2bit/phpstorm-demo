278eee537542ce7a5dcba27e5272762dcfc6efef
Rene Gerritsen <rene.gerritsen@check24.de>
2017-05-23 14:07:43 +0200

    extend model

---
+++ Model/FamilyPizza.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Model;
+
+/**
+ * Family Pizza ! nothing more to say !
+ *
+ * @todo overwrite constructor and add Field size
+ *
+ * @copyright CHECK24 Vergleichsportal Reise GmbH
+ */
+class FamilyPizza extends Pizza
+{
+}
